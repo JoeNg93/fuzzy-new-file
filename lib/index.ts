c7a69246780d7437e96158c45d045c53c2f3b402
Joe Nguyen <ntuandung93@gmail.com>
2020-06-19 23:32:15 +0200

    feat: Check Node version to be at least v10 (#3)

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,26 @@
 #!/usr/bin/env node
 
 import yargs from 'yargs';
+import semver from 'semver';
 
 import { setupCommand } from './command';
 
-yargs.command(setupCommand()).help().argv;
+function nodeVersionHigherOrEqual(minVersion: string) {
+  return semver.gte(
+    process.version,
+    semver.coerce(minVersion) as semver.SemVer
+  );
+}
+
+function main() {
+  if (!nodeVersionHigherOrEqual('v10')) {
+    process.stderr.write(
+      `ERROR: This CLI requires Node v10 or above, your current version: ${process.version}\n`
+    );
+    process.exit(1);
+  }
+
+  yargs.command(setupCommand()).help().argv;
+}
+
+main();
